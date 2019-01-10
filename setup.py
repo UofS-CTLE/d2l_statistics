9c30f4ca056c261e63a3f7cbb92abd79f449ef59
Sean Batzel <romulus108@protonmail.com>
2019-01-10 09:56:30 -0500

    v0.1.0 - ready for review.
    Signed-off-by: Sean Batzel <romulus108@protonmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='d2l_statistics',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Sean Batzel, Daniel Ricker',
+    author_email='sean.batzel@scranton.edu, daniel.ricker@scranton.edu',
+    description='', install_requires=['weasyprint']
+)
